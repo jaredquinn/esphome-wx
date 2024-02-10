3a35ec00eb6308219d4aedf405412b7f4a56f1e1
Jared Quinn <jared@jaredquinn.info>
2024-02-10 16:49:05 +1100

    More microphone tweaks for the AI Tinker Audio Dev Kit voice assistant

--- components/es8388/es8388_component.cpp
+++ components/es8388/es8388_component.cpp
@@ -54,73 +54,56 @@ void ES8388Component::mode_default() {
 }
 
 void ES8388Component::mode_voice_recording() {
-  this->write_byte(0x09, 0x77); //  +21dB : recommended value for ALC & voice recording
-// ALC Config (as recommended by ES8388 user guide for voice recording)
-  this->write_byte(0x12, 0xe2); // Reg 0x12 = 0xe2 (ALC enable, PGA Max. Gain=23.5dB, Min. Gain=0dB)
-  this->write_byte(0x13, 0xa0); // Reg 0x13 = 0xa0 (ALC Target=-1.5dB, ALC Hold time =0 mS)
-  this->write_byte(0x14, 0x12); // Reg 0x14 = 0x12(Decay time =820uS , Attack time = 416 uS)
-  this->write_byte(0x15, 0x06); // Reg 0x15 = 0x06(ALC mode)
-  this->write_byte(0x16, 0xc3); // Reg 0x16 = 0xc3(nose gate = -40.5dB, NGG = 0x01(mute ADC))
+  this->write_byte(ES8388_ADCCONTROL1, 0x77); //  +21dB : recommended value for ALC & voice recording
+  // ALC Config (as recommended by ES8388 user guide for voice recording)
+  this->write_byte(ES8388_ADCCONTROL10, 0xe2); // Reg 0x12 = 0xe2 (ALC enable, PGA Max. Gain=23.5dB, Min. Gain=0dB)
+  this->write_byte(ES8388_ADCCONTROL11, 0xa0); // Reg 0x13 = 0xa0 (ALC Target=-1.5dB, ALC Hold time =0 mS)
+  this->write_byte(ES8388_ADCCONTROL12, 0x12); // Reg 0x14 = 0x12(Decay time =820uS , Attack time = 416 uS)
+  this->write_byte(ES8388_ADCCONTROL13, 0x06); // Reg 0x15 = 0x06(ALC mode)
+  this->write_byte(ES8388_ADCCONTROL14, 0xc3); // Reg 0x16 = 0xc3(nose gate = -40.5dB, NGG = 0x01(mute ADC))
 }
 
 void ES8388Component::setup() {
   PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0_CLK_OUT1);
   WRITE_PERI_REG(PIN_CTRL, READ_PERI_REG(PIN_CTRL) & 0xFFFFFFF0);
 
-  this->write_byte(0x19, 0x04); // mute
+  this->write_byte(ES8388_DACCONTROL3, 0x04); // mute
   this->write_byte(ES8388_MASTERMODE, 0x00); // worker mode
   this->write_byte(ES8388_CHIPPOWER, 0xFF);  // Power down DEM/STM
   this->write_byte(ES8388_CONTROL1, 0x05);   // Set Chip to Play&Record Mode
   this->write_byte(ES8388_CONTROL2, 0x40);   // Power Up Analog and Ibias
 
-  // DAC powerdown
-  //this->write_byte(ES8388_DACPOWER, 0xC0);
   this->write_byte(ES8388_DACPOWER, 0x3C);  // Power Up DAC& enable Lout/Rout
-  // vmidsel/500k ADC/DAC idem
-  //this->write_byte(0x00, 0x12);
+  //this->write_byte(0x00, 0x12); // vmidsel/500k ADC/DAC idem
 
-  // i2s 16 bits
   this->write_byte(ES8388_DACCONTROL1, 0x18); // SFI setting (i2s mode/16 bit)
   this->write_byte(ES8388_DACCONTROL2, 0x02); //  DAC MCLK/LCRK ratio (256)
 
-  // LIN2/RIN2 for mixer
-  this->write_byte(0x26, 0x00);
-  // left DAC to left mixer
-  this->write_byte(0x27, 0x90);
-  // right DAC to right mixer
-  this->write_byte(0x2A, 0x90);
-  // DACLRC ADCLRC idem
-  this->write_byte(0x2B, 0x80);
-  this->write_byte(0x2D, 0x00);
+  this->write_byte(0x26, 0x00); // LIN2/RIN2 for mixer
+  this->write_byte(0x27, 0x90); // left DAC to left mixer
+  this->write_byte(0x2A, 0x90); // right DAC to right mixer
+  this->write_byte(ES8388_DACCONTROL21, 0x80); // DACLRC ADCLRC idem
+  this->write_byte(ES8388_DACCONTROL23, 0x00);
   // DAC volume max
   this->write_byte(0x1B, 0x00);
   this->write_byte(0x1A, 0x00);
 
-  // ADC poweroff
-  this->write_byte(0x03, 0xFF);
-
-  // LINPUT1/RINPUT1
+  this->write_byte(ES8388_ADCPOWER, 0xFF);    // ADC poweroff
   this->write_byte(ES8388_ADCCONTROL2, 0x50); // MIC2
-  // ADC mono left
-  this->write_byte(ES8388_ADCCONTROL3, 0x02);
-  // i2S 16b
+  this->write_byte(ES8388_ADCCONTROL3, 0x02); // ADC mono left
   this->write_byte(ES8388_ADCCONTROL4, 0x0D); // / Left/Right data, Left/Right justified mode, Bits length, I2S format
   this->write_byte(ES8388_ADCCONTROL5, 0x02); // ADCFsMode,singel SPEED,RATIO=256
-  // ADC Volume
-  this->write_byte(0x10, 0x00);
+  this->write_byte(0x10, 0x00); // ADC Volume
   this->write_byte(0x11, 0x00);
-  // ALC OFF
-  this->write_byte(0x03, 0x09);
+  this->write_byte(0x03, 0x09); // ALC OFF
   this->write_byte(0x2B, 0x80);
 
   this->write_byte(0x02, 0xF0);
   delay(1);
   this->write_byte(0x02, 0x00);
-  // DAC power-up LOUT1/ROUT1 enabled
-  this->write_byte(0x04, 0x30);
+  this->write_byte(0x04, 0x30); // DAC power-up LOUT1/ROUT1 enabled
   this->write_byte(0x03, 0x00);
-  // DAC volume max
-  this->write_byte(0x2E, 0x1C);
+  this->write_byte(0x2E, 0x1C); // DAC volume max
   this->write_byte(0x2F, 0x1C);
 
   this->write_byte(ES8388_CHIPPOWER, 0x00); // power up chip
